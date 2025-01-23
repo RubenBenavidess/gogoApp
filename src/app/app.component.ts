d8b7c763f55f4955828832395620d8c915f27cd3
Joan Oswaldo Cobeña Zambrano <joancobe64@gmail.com>
2025-01-23 16:15:04 -0500

    Configuracion de prime Ng, y TailwindCSS, task creator front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { TaskCreatorComponent } from "./components/task-creator/task-creator.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DialogModule, TaskCreatorComponent, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'gogoApp';
+  taskCreatorVisible: boolean = false;
+
+  showTaskCreator(): void{
+    this.taskCreatorVisible = true;
+  }
 }
--- src/app/components/task-creator/task-creator.component.ts
+++ src/app/components/task-creator/task-creator.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { SelectModule } from 'primeng/select';
+import { Button } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import {InputTextModule} from 'primeng/inputtext';
+import {CalendarModule} from 'primeng/calendar';
+import { Proyecto } from '../../models/proyecto';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-task-creator',
-  imports: [DialogModule, SelectModule],
+  standalone: true,
+  imports: [DialogModule, SelectModule, Button, FormsModule, InputTextModule, CalendarModule],
   templateUrl: './task-creator.component.html',
   styleUrl: './task-creator.component.css'
 })
 export class TaskCreatorComponent {
+  proyectos: Proyecto[] = [new Proyecto(0, "Default Project", [], [])];
+  proyectoTarea: Proyecto = this.proyectos[0]; 
+  prioridades: string[] = ["Urgente", "Alta", "Media", "Baja"];
+  prioridadTarea: string = this.prioridades[this.prioridades.length/2];
+  nombreTarea: string = "Tarea";
+  fechaActual: Date = new Date();
+  rangoFechas: Date[] = [];
 
 }
