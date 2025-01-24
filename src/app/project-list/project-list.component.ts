2c7815a62205b43cb21595c6becb356c5c3d04dc
Ruben Dario Benavides Macias <dario.benavides@hotmail.com>
2025-01-23 19:20:14 -0500

    Componente Lista de Proyectos

---
+++ src/app/project-list/project-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ProyectosService } from '../services/proyectos-service.service';
+import { AccordionModule } from 'primeng/accordion';
+import { Proyecto } from '../models/proyecto';
+import { TaskListComponent } from '../components/task-list/task-list.component';
+
+@Component({
+  selector: 'app-project-list',
+  imports: [AccordionModule, TaskListComponent],
+  templateUrl: './project-list.component.html',
+  styleUrl: './project-list.component.css'
+})
+export class ProjectListComponent {
+
+  proyectos: Proyecto[] = [];
+
+  constructor(private proyectosService: ProyectosService) { }
+
+  ngOnInit() {
+    this.proyectos = this.proyectosService.getProyectos();
+  }
+  
+}
